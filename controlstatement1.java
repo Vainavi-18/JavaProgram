130d5f087f5516d1723d9730e5f23a12f4504b58
Vainavi-18 <vainavig.23ece@kongu.edu>
2026-04-29 11:25:44 +0530

    Control statement if else
    Signed-off-by: Vainavi-18 <vainavig.23ece@kongu.edu>

---
+++ controlstatement1.java
@@ -0,0 +1,12 @@
+public class controlstatement1 {
+    public static void main(String args[]){
+        int a=10;
+        if(a>5){
+            System.out.println("a is greater than 5");
+        }
+        else{
+            System.out.println("a is less than or equal to 5");
+        }
+    }
+    
+}
